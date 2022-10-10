f848c42c970b973bbd1f9428700d92ea0894b87d
yongseanliu <yongseanliu@tencent.com>
2022-10-10 10:15:25 +0800

    slice add All

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -22,3 +22,23 @@ func InBy[T any](slice []T, fn delegate.FuncTBool[T]) bool {
 	}
 	return false
 }
+
+// All 判断切片里所有元素等于比较数值?
+func All[T comparable](slice []T, want T) bool {
+	for _, v := range slice {
+		if v != want {
+			return false
+		}
+	}
+	return true
+}
+
+// AllBy 通过回调函数,判断切片里所有元素满足条件
+func AllBy[T any](slice []T, fn delegate.FuncTBool[T]) bool {
+	for _, v := range slice {
+		if !fn(v) {
+			return false
+		}
+	}
+	return true
+}
--- pkg/slice/slice_test.go
+++ pkg/slice/slice_test.go
@@ -89,3 +89,87 @@ func TestInBy(t *testing.T) {
 		})
 	}
 }
+
+func TestAll(t *testing.T) {
+	type args[T comparable] struct {
+		slice []T
+		want  T
+	}
+	tests := []struct {
+		name string
+		args args[int]
+		want bool
+	}{
+		{
+			name: "all equal",
+			args: args[int]{
+				slice: []int{2, 2, 2, 2, 2, 2, 2},
+				want:  2,
+			},
+			want: true,
+		},
+		{
+			name: "not equal",
+			args: args[int]{
+				slice: []int{1, 2, 3, 4, 5, 6, 7},
+				want:  100,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := All(tt.args.slice, tt.args.want); got != tt.want {
+				t.Errorf("All() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAllBy(t *testing.T) {
+	type data struct {
+		Name string
+		Age  uint
+	}
+	dataList := []data{
+		{Name: "Peter", Age: 10},
+		{Name: "Andy", Age: 20},
+		{Name: "Sally", Age: 15},
+	}
+	type args[T any] struct {
+		slice []T
+		fn    delegate.FuncTBool[T]
+	}
+	tests := []struct {
+		name string
+		args args[data]
+		want bool
+	}{
+		{
+			name: "exists",
+			args: args[data]{
+				slice: dataList,
+				fn: func(d data) bool {
+					return d.Age < 30
+				},
+			},
+			want: true,
+		},
+		{
+			name: "not exists",
+			args: args[data]{
+				slice: dataList,
+				fn: func(d data) bool {
+					return d.Name == "Stupid"
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AllBy(tt.args.slice, tt.args.fn); got != tt.want {
+				t.Errorf("AllBy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
